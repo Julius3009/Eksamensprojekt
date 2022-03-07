eaabc79041bcfab13ff860a27f8c3282a488d500
Julius Olsen <juliuso597@gmail.com>
2022-03-07 09:54:04 +0100

    Initial Commit

---
+++ Programmering Eksamensprojekt/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Programmering Eksamensprojekt
+//
+//  Created by Julius Olsen on 07/03/2022.
+//  Copyright © 2022 Julius Olsen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
