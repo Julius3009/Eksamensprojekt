f3fa52a93e74e93e960e2edfcac56a3a5d0e761c
Julius Olsen <juliuso597@gmail.com>
2022-05-15 20:56:13 +0200

    hallah

--- Programmering Eksamensprojekt/main.cpp
+++ Programmering Eksamensprojekt/main.cpp
@@ -23,11 +23,12 @@ char boardSolved [9][9] = {{'8','4','6','1','7','2','5','9','3'},
                            {'3','5','4','2','8','6','9','1','7'},
                            {'6','1','7','5','9','3','2','8','4'}};
 
+
+
 void placerTal(char tal, int y, int x);
 bool slutSpil = false;
 bool validerInput (char tal, int y, int x);
 void check_filled();
-
 void drawboard (){
 
     cout << "-------------------------------------" << endl;
@@ -48,9 +49,50 @@ void drawboard (){
 
 int main()
 {
+    int svar;
+    char svar2;
+    char tal;
+    int x, y;
+    
+    cout << "Velkommen til Sudoku" << endl;
+    
+    cout << "Tast 1 for at se reglernerne" << endl;
+    cout << "Tast 2 for at spille spillet" << endl;
+
+    cin >> svar;
+    
+    if ((svar = 1)){
+        cout << "Soduko pladen " << endl;
+        
+        cout << "For at starte spillet, indtast et tilfældigt bogstav eller tegn" << endl;
+        
+        cin >> svar2;
+        
+        while (slutSpil == false) {
+            
+            
+            drawboard();
+            
+            cout << "Indtast det tal du vil placere" << endl;
+            cin >> tal;
+            
+            cout << "Indtast koordinaterne til det felt du vil placere dit tal i";
+            cin >> y; cin >> x;
+            
+            y--;
+            x--;
+                       placerTal(tal, y, x);
+            
+
+        }
+        
+    } else {
+        
+    
+    
+    
     while (slutSpil == false) {
-        char tal;
-        int x, y;
+      
         
         drawboard();
         
@@ -59,103 +101,35 @@ int main()
         
         cout << "Indtast koordinaterne til det felt du vil placere dit tal i";
         cin >> y; cin >> x;
-
         
-        if(validerInput(tal, y, x)){
-                   placerTal(tal, y-1, x-1);
-
-               } else {
-                   cout << "NEJ, kan ikke placeres her!" << endl;
-               }
-               cout << endl << endl;
-        
+        y--;
+        x--;
+                   placerTal(tal, y, x);
         
 
     }
     
-  
+    }
      
     
     
 
 }
 
-void check_filled() {
-    int spots_not_filled = 0;
-    for(int i = 0; i<9; i++){
-        for(int j = 0; j<9; j++){
-            if (board[i][j] == ' ') {
-                spots_not_filled++;
-            }
-        }
-    }
-    if (spots_not_filled == 0) {
-        slutSpil = true;
-    }
-    
-}
 
 
 void placerTal(char tal , int y, int x){
-//board[y][x]=tal;
 
    if (board[x][y] == ' '){
     board[x][y] = tal;
-   }
+   
+    } else {
+          cout << "Dit valgte tal kan ikke placeres i dette felt" << endl;
+     }
+      cout << endl << endl;
 
     }
 
-
-
-//     Tjek vandret for samme tal
-bool validerInput (char tal, int y, int x){
-    for(int valider = 0; valider < 9; valider++){
-        if(valider != x){
-            if(board[valider][y] == tal){
-                return false;
-            }
-        }
-    }
-// Tjek lodret for samme tal
-    for(int valider = 0; valider < 9; valider++){
-        if(valider != y){
-            if(board[x][valider] == tal){
-            }
-        }
-    }
-    int board_tjek_x, board_tjek_y;
-        if(x < 3){
-            board_tjek_x = 0;
-        }
-        else if(x < 6){
-            board_tjek_x = 3;
-        }
-        else {
-            board_tjek_x = 6;
-        }
-        if(y < 3){
-            board_tjek_y = 0;
-        }
-        else if(y < 6){
-            board_tjek_y = 3;
-        }
-        else {
-            board_tjek_y = 6;
-        }
-
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                int x = board_tjek_x + i;
-                int y = board_tjek_y + j;
-                if(x != x && y != y){
-                    if(board [x][y] == tal)
-                    return false;
-                }
-            }
-        }
-        return true;
-    
-    }
 
 
 
